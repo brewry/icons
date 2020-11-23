74a4b0ce8af58136842596b25199c6e864f96f8c
Chris Magnussen <chris@hirvi.no>
2020-11-23 13:45:21 +0100

    Added typings

---
+++ types/mixin.d.ts
@@ -0,0 +1,29 @@
+export namespace props {
+    export const size: (StringConstructor | NumberConstructor)[];
+    export const color: StringConstructor;
+}
+export namespace computed {
+    export function listeners(): any;
+    export function listeners(): any;
+    export function styles(): {
+        color: any;
+        height: any;
+        width: any;
+    } | {
+        color: any;
+        height?: undefined;
+        width?: undefined;
+    };
+    export function styles(): {
+        color: any;
+        height: any;
+        width: any;
+    } | {
+        color: any;
+        height?: undefined;
+        width?: undefined;
+    };
+    export function attrs(): any;
+    export function attrs(): any;
+}
+//# sourceMappingURL=mixin.d.ts.map
